afa9c5066bc02c143b3b3c9923e0102d447a3cd7
Mrinal Wadhwa <mrinal@ockam.io>
2022-05-24 07:24:26 -0700

    build: ockam v0.59.0

--- ockam.rb
+++ ockam.rb
@@ -8,28 +8,28 @@ class Ockam < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       @@target = "aarch64-apple-darwin"
-      sha256 "a3ebd41d28819c218deba0dd8021d9385f753a76cf97284bd244fb19d6081ec6"
+      sha256 "ac521c212054ce37b626a8fc6b925d594eb7ad79aecae8009e5abb45de4e1820"
     end
 
     if Hardware::CPU.intel?
       @@target = "x86_64-apple-darwin"
-      sha256 "6a29a743418f2fbcefa0004d6a1e055fa5d0a497d15cca7ec3d0d642a36e50ad"
+      sha256 "9e652ce640034a0d4359911ed78400f72b5a5e7168ec510f1c23edd17eab3a95"
     end
   end
 
   if OS.linux?
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       @@target = "aarch64-unknown-linux-gnu"
-      sha256 "380bf150cf232f4c0ed0ee77c3339fbf0fb0b635970f6ffa3e3adca96e422f10"
+      sha256 "1b800b3f8ae7e16dc7211499833888cf0eec2e6b02f7fce274b986e77ec0ec54"
     end
 
     if Hardware::CPU.intel?
       @@target = "x86_64-unknown-linux-gnu"
-      sha256 "1b024013f9d8b3ebce2ae70583651ecef14e5970dc6689043b2e292f331f1f41"
+      sha256 "8e2c3879fbe90dd5fac739fe70aed260effc18d65c66153ad9a5a32056881b51"
     end
   end
 
-  url "#{homepage}/releases/download/ockam_v0.58.0/ockam.#{@@target}"
+  url "#{homepage}/releases/download/ockam_v0.59.0/ockam.#{@@target}"
 
   def install
     if !@@target.nil?
