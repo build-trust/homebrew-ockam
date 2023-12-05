d04fd4042d55e43f958dc48e9fe80d048d370a84
ockam-team <dev@ockam.io>
2023-12-05 21:02:47 +0000

    ci: update to release ockam_v0.106.0

--- Casks/ockam-app.rb
+++ Casks/ockam-app.rb
@@ -1,17 +1,17 @@
 cask "ockam-app" do
-  version "ockam_v0.105.0"
+  version "ockam_v0.106.0"
   name "Ockam"
   desc "End-to-end encryption and mutual authentication for distributed applications"
   homepage "https://github.com/build-trust/ockam"
 
   if Hardware::CPU.arm?
-    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.105.0/ockam.app.aarch64-apple-darwin.dmg"
-    sha256 "cce407a3a47ae337fbe37dce84d4c2f3fadd7a06f1380b903c689ab2f066ddb8"
+    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.106.0/ockam.app.aarch64-apple-darwin.dmg"
+    sha256 "90b4d373a5eecf2c90a313d14735d625dc577bbe2934731363cbee2f20fb9fdb"
    end
 
   if Hardware::CPU.intel?
-    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.105.0/ockam.app.x86_64-apple-darwin.dmg"
-    sha256 "042966c97ee103fdc83543efb0a24fe871d407ba5ec47f6360cd6ecedb227fb3"
+    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.106.0/ockam.app.x86_64-apple-darwin.dmg"
+    sha256 "962bf88b9e4855d32d17a7c2b1d66c3083530d6c041785639ff570b1a417520a"
   end
 
   depends_on formula: "ockam"
