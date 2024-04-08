0d645a14547faa98954893dc4eef7ef4bd41cb74
ockam-team <dev@ockam.io>
2024-04-08 23:21:33 +0000

    ci: update to release ockam_v0.121.0

--- Casks/portals.rb
+++ Casks/portals.rb
@@ -1,17 +1,17 @@
 cask "portals" do
-  version "ockam_v0.120.0"
+  version "ockam_v0.121.0"
   name "Portals, by Ockam"
   desc "Privately share TCP services from your Mac with anyone, anywhere."
   homepage "https://github.com/build-trust/ockam"
 
   if Hardware::CPU.arm?
-    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.120.0/ockam.app.aarch64-apple-darwin.dmg"
-    sha256 "1c8c229bc2f39639a4f717989daf44289186cdd5ebed00e58b70e8a8ea320b8b"
+    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.121.0/ockam.app.aarch64-apple-darwin.dmg"
+    sha256 "212c3f3f6076b2978c1959e7407fcd005630cd99e13b62f5ab9b846d88924c5b"
    end
 
   if Hardware::CPU.intel?
-    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.120.0/ockam.app.x86_64-apple-darwin.dmg"
-    sha256 "5529c3900c6b2fb07ed826caf14bcec18af83a7cef08791aa40ae8c652199d60"
+    url "https://github.com/build-trust/ockam/releases/download/ockam_v0.121.0/ockam.app.x86_64-apple-darwin.dmg"
+    sha256 "4c25ff86520c6dd0c75624ff39fde1ecf266211ebbc0c8df399f6ca5fc2955a1"
   end
 
   depends_on formula: "ockam"
--- ockam.rb
+++ ockam.rb
@@ -8,8 +8,8 @@ class Ockam < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.120.0/ockam.aarch64-apple-darwin"
-      sha256 "67e45bd955d76478de09e89373b5ce22aaa931169a8575c943ed2eb52e50f278"
+      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.121.0/ockam.aarch64-apple-darwin"
+      sha256 "199c9de8d575cbfe704844b17ffa5cd7b2db7b976cdc39158589c8292d69d424"
 
       def install
         bin.install "ockam.aarch64-apple-darwin" => "ockam"
@@ -19,8 +19,8 @@ class Ockam < Formula
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.120.0/ockam.x86_64-apple-darwin"
-      sha256 "f178fd0f0b281584dd5a4c8f36c80935f189b334039587b3fcc7d21d44a27a20"
+      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.121.0/ockam.x86_64-apple-darwin"
+      sha256 "78287d15700fb32d9456814ce37497f1ba6c40c08e6d515e5baad4f24627a866"
 
       def install
         bin.install "ockam.x86_64-apple-darwin" => "ockam"
@@ -32,8 +32,8 @@ class Ockam < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.120.0/ockam.aarch64-unknown-linux-musl"
-      sha256 "afa4ebe9d2309e7fa25f2875bc27b8cb32b23e1fdb1fdcf5d9a330ae5114d030"
+      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.121.0/ockam.aarch64-unknown-linux-musl"
+      sha256 "4b0ee9e6642616456bb3900627044a592acbad3c77fba6aaa48a80504c1cc1c4"
 
       def install
         bin.install "ockam.aarch64-unknown-linux-musl" => "ockam"
@@ -43,8 +43,8 @@ class Ockam < Formula
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.120.0/ockam.x86_64-unknown-linux-musl"
-      sha256 "fa11a3320b0852c8db3a8f7a6362d1de49d63edd8428f8df9066070f13705ee8"
+      url "https://github.com/build-trust/ockam/releases/download/ockam_v0.121.0/ockam.x86_64-unknown-linux-musl"
+      sha256 "21c6579b9a80ef7dc412527ece9a3592014cfbcd67607052b49dca7b2bf2dc94"
 
       def install
         bin.install "ockam.x86_64-unknown-linux-musl" => "ockam"
